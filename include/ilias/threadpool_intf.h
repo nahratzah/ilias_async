2943a36e52b30da9a29b14c909703d2b2a72c838
Ariane van der Steldt <ariane@stack.nl>
2013-05-05 18:11:41 +0200

    Copyright + sort includes.

--- include/ilias/threadpool_intf.h
+++ include/ilias/threadpool_intf.h
@@ -1,7 +1,27 @@
-#include <ilias/refcnt.h>
+/*
+ * Copyright (c) 2013 Ariane van der Steldt <ariane@stack.nl>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifndef ILIAS_THREADPOOL_INTF_H
+#define ILIAS_THREADPOOL_INTF_H
+
 #include <ilias/ll.h>
-#include <utility>
+#include <ilias/refcnt.h>
+#include <ilias/util.h>
 #include <climits>
+#include <utility>
+#include <type_traits>
 
 namespace ilias {
 
@@ -504,4 +524,6 @@ public:
 };
 
 
-}
+} /* namespace ilias */
+
+#endif /* ILIAS_THREADPOOL_INTF_H */
