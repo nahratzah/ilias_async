d50de9b490b847a106383f9faa1c0e88a5dfaa6f
Ariane van der Steldt <ariane@stack.nl>
2013-05-09 15:37:55 +0200

    Implement race condition wakeup test for tp_service_set.

---
+++ test/threadpool_intf/race_wakeup.cc
@@ -0,0 +1,198 @@
+#include <ilias/threadpool_intf.h>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <iostream>
+#include <cassert>
+
+
+class client
+{
+public:
+	enum state {
+		FIRST,
+		WAKEUP,
+		SECOND,
+		SECOND_DONE
+	};
+
+	state m_state{ FIRST };
+	std::mutex m_lck;
+	std::condition_variable m_cnd;
+	std::mutex wakeup_block;
+	std::condition_variable wakeup_cnd;
+	bool wakeup_signaled{ false };
+
+	client()
+	{
+		/* Empty body. */
+	}
+
+	class threadpool_client
+	:	public virtual ilias::threadpool_client_intf
+	{
+	public:
+		client* m_client;
+
+	private:
+		std::thread m_thr;
+
+		void
+		thread_fn() noexcept
+		{
+			std::lock_guard<std::mutex> block {
+				this->m_client->wakeup_block
+			};
+			std::unique_lock<std::mutex> guard {
+				this->m_client->m_lck
+			};
+			auto& m_state = this->m_client->m_state;
+			auto& cnd = this->m_client->m_cnd;
+
+			while (m_state != WAKEUP)
+				cnd.wait(guard);
+
+			m_state = SECOND;
+			this->wakeup(1);
+
+			this->m_client->wakeup_signaled = true;
+			this->m_client->wakeup_cnd.notify_all();
+		}
+
+	public:
+		threadpool_client(client* c)
+		:	m_client(c),
+			m_thr{ &threadpool_client::thread_fn, this }
+		{
+			/* Empty body. */
+		}
+
+		~threadpool_client() noexcept
+		{
+			this->m_thr.join();
+		}
+
+	protected:
+		bool
+		do_work() noexcept
+		{
+			if (!this->m_client)
+				return false;
+
+			bool rv;
+			switch (this->m_client->m_state) {
+			case FIRST:
+				rv = true;
+				this->m_client->m_state = WAKEUP;
+				break;
+			case SECOND:
+				rv = true;
+				this->m_client->m_state = SECOND_DONE;
+				break;
+			case WAKEUP:
+				{
+					std::cerr << "Notifying thead to send wakeup." << std::endl;
+					std::lock_guard<std::mutex> guard {
+						this->m_client->m_lck
+					};
+					this->m_client->m_cnd.notify_all();
+				}
+				rv = false;
+				break;
+			case SECOND_DONE:
+				rv = false;
+				this->m_client->ptr = nullptr;
+			}
+			return rv;
+		}
+
+		bool
+		has_work() noexcept
+		{
+			switch (this->m_client->m_state) {
+			default:
+				break;
+			case FIRST:
+			case SECOND:
+				return true;
+			}
+			return false;
+		}
+
+		void
+		on_service_detach() noexcept
+		{
+			if (this->m_client)
+				this->m_client->ptr = nullptr;
+		}
+	};
+
+	ilias::threadpool_client_ptr<threadpool_client> ptr;
+
+	~client() noexcept
+	{
+		if (this->ptr)
+			this->ptr->m_client = nullptr;
+	}
+
+	void
+	attach(ilias::threadpool_client_ptr<threadpool_client> ptr)
+	{
+		this->ptr = std::move(ptr);
+	}
+
+	client*
+	threadpool_client_arg()
+	{
+		return this;
+	}
+};
+
+
+bool
+race_test() noexcept
+{
+	client c;
+	ilias::tp_service_set s;
+	bool race;
+
+	threadpool_attach(c, s);
+
+	while (s.has_work())
+		while (s.do_work());
+
+	{
+		/* Wait until wakeup fires... */
+		std::cerr << "Waiting for wakeup to complete..." << std::endl;
+		std::unique_lock<std::mutex> block {
+			c.wakeup_block
+		};
+		race = c.wakeup_signaled;	/* Race detection. */
+		while (!c.wakeup_signaled)
+			c.wakeup_cnd.wait(block);
+	}
+
+	while (s.has_work())
+		while (s.do_work());
+
+	assert(s.has_work());
+	return race;
+}
+
+
+int
+main()
+{
+	bool race = false;
+	int i;
+	for (i = 0; !race && i < 1000; ++i)
+		race = race_test();
+
+	if (race) {
+		std::cout << "Race detected after " << i << " attempts."
+		    << std::endl;
+		return 0;
+	}
+	std::cout << "Failed to trigger race." << std::endl;
+	return 1;
+}
