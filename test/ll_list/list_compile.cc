c431154f7df266a6eda9b67f9f25a201c1c8b5ca
Ariane van der Steldt <ariane@stack.nl>
2013-07-13 01:44:31 +0100

    Add simple ll_list testcase, to see if the basic functionality is there.

---
+++ test/ll_list/list_compile.cc
@@ -0,0 +1,29 @@
+#include <ilias/ll_list.h>
+#include <ilias/refcnt.h>
+#include <iostream>
+
+
+class foo
+:	public ilias::ll_list_hook<>,
+	public ilias::refcount_base<foo>
+{
+};
+
+using foo_list = ilias::ll_smartptr_list<foo>;
+
+
+int
+main()
+{
+	foo_list fl;
+	fl.push_back(ilias::make_refpointer<foo>());
+	fl.push_front(ilias::make_refpointer<foo>());
+
+	for_each(fl.cbegin(), fl.cend(), [](const foo& e) {
+		std::cout << "Element at address " << &e << std::endl;
+	    });
+
+	fl.erase(fl.begin());
+
+	return 0;
+}
